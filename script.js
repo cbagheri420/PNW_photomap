566b819c4631f73e44eb0e29a79f8e6af8e021e0
cbagheri420 <cameron.bagheri20@gmail.com>
2025-04-16 15:53:36 -0700

    2 real markers
    correct images and coordinates

--- script.js
+++ script.js
@@ -7,5 +7,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Add a sample photo marker (you’ll load from Firebase later)
-const marker = L.marker([47.6062, -122.3321]).addTo(map);
-marker.bindPopup('<strong>Seattle</strong><br><img src="images/spaceneedletest.jpg" alt="Seattle photo" width="150">');
+const marker1 = L.marker([47.49761227227601, -121.79088692588667]).addTo(map);
+marker1.bindPopup('<strong>North Bend, Snoqualmie River</strong><br><img src="images/northbend_1.jpg" alt="Snoqualmie River photo" width="150">');
+
+const marker2 = L.marker([47.65295214807401, -122.30632969624457]).addTo(map);
+marker2.bindPopup('<strong>University of Washington, Cherry Blossoms</strong><br><img src="images/udub_1.jpg" alt="UDub photo" width="150">');
